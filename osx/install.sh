513c05958884f603e9b286daefb09adb0254a50e
Justin Hilliard <justin.hilliard@sonos.com>
2018-12-17 23:22:38 -0800

    added tap touch mouse pad

--- osx/install.sh
+++ osx/install.sh
@@ -102,3 +102,8 @@ sudo nvram SystemAudioVolume=” “
 defaults write com.apple.finder ShowPathbar -bool true
 #Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
+# Enable tap touch for trackpad
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
