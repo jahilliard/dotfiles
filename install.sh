5aa94d77b77b5aca94410c156946140cdea08cb4
Justin Hilliard <justin.a.hilliard@gmail.com>
2026-04-08 14:55:53 -0400

    added example env

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash/env_example ~/.env_example
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 mkdir -p ~/.config/nvim/
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
